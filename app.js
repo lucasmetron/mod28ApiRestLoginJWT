9ba26cfe6f8bafee33fb8c059756c8d5a4889cd1
lucasmetron <lucasmetron@gmail.com>
2021-06-11 15:14:06 -0300

    conectando com o atlas

--- app.js
+++ app.js
@@ -2,6 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const userRouter = require('./routes/userRouter');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URL_CONNECTION,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
+
+    (error) => {
+        if (error) {
+            console.log(error)
+        } else {
+            console.log("Mongo Connected")
+        }
+
+    }
+)
 
 app.use('/user', userRouter);
 
