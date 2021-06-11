bf9a0b45c64b2ef1c5d22e3efe9ca40337bfc68f
lucasmetron <lucasmetron@gmail.com>
2021-06-11 14:10:14 -0300

    configuração noden

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello Wolrd")
+    res.send(process.env.TESTE)
 })
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server running`)
+    console.log(`server running on port ${process.env.PORT}`)
 })
+
+
 
