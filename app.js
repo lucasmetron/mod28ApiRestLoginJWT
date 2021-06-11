a172e057033cab9c0f5dd5bc69a0ed75109ebf58
lucasmetron <lucasmetron@gmail.com>
2021-06-11 14:41:11 -0300

    separando rotas e controller

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const userRouter = require('./routes/userRouter');
 
-app.get('/', (req, res) => {
-    res.send(process.env.TESTE)
-})
+app.use('/user', userRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on port ${process.env.PORT}`)
-})
+});
 
 
 
