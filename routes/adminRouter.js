087bbd940710c5fd573980caac33991be90b4604
lucasmetron <lucasmetron@gmail.com>
2021-06-25 10:55:59 -0300

    protegendo rota

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -4,7 +4,13 @@ const router = express.Router();
 const auth = require('../controllers/authController')
 
 router.get('/', auth, (req, res) => {
-    res.send("Esse dado só deve ser visto pelo admin")
-})
+
+    if (req.user.admin) {
+        res.send("Esse dado só deve ser visto pelo admin")
+    } else {
+        res.status(401).send("User not Admin: Acess Denied")
+    }
+
+});
 
 module.exports = router;
