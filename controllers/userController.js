6b97d15f66e450d8f90bbb14fb8048c02788d9d0
lucasmetron <lucasmetron@gmail.com>
2021-06-25 11:41:04 -0300

    joi

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,8 +2,16 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const { loginValidate, registerValidate } = require('./validate')
+
+
 const userController = {
     register: async function (req, res) {
+
+        const { error } = registerValidate(req.body)
+        if (error) {
+            return res.status(400).send(error.message)
+        }
 
         const selectedUser = await User.findOne({ email: req.body.email })
 
@@ -27,6 +35,12 @@ const userController = {
     },
 
     login: async function (req, res) {
+
+        const { error } = loginValidate(req.body)
+        if (error) {
+            return res.status(400).send(error.message)
+        }
+
         const selectedUser = await User.findOne({ email: req.body.email })
 
         if (!selectedUser) {
