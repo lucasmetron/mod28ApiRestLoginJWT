5bd34fa376c21327bce844ff727862b9e18ef4bc
lucasmetron <lucasmetron@gmail.com>
2021-06-25 10:29:27 -0300

    protegendo rotas

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const userRouter = require('./routes/userRouter');
+const adminRouter = require('./routes/adminRouter');
 const mongoose = require('mongoose');
+
 
 mongoose.connect(process.env.MONGO_URL_CONNECTION,
     {
@@ -21,6 +23,8 @@ mongoose.connect(process.env.MONGO_URL_CONNECTION,
 )
 
 app.use('/user', express.json(), userRouter);
+
+app.get('/admin', express.json(), adminRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on port ${process.env.PORT}`)
